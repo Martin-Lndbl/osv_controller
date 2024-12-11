05dfb38c6b232b5a5df79b6fef8336668edf1c20
Martin Lindbüchl <lblsolutions@outlook.de>
2024-12-11 08:05:54 +0000

    improve gen_compile_commands for osv

--- pkgs/gen_compile_commands.nix
+++ pkgs/gen_compile_commands.nix
@@ -1,8 +1,24 @@
 {
   writeScriptBin,
+  python312Packages,
 }:
 
-writeScriptBin "gen_compile_commands" (builtins.readFile ./gen_compile_commands.sh)
+writeScriptBin "osv_compile_commands" ''
+  #!/bin/bash
+
+  if [ -z "$1" ]; then
+    echo "You need to supply an image name as argument"
+    exit 1
+  fi
+
+  make_output=$(mktemp)
+
+  ./scripts/build clean
+  ./scripts/build image=$1 V=1 > $make_output
+  ${python312Packages.compiledb}/bin/compiledb --parse $make_output
+''
+
+# writeScriptBin "gen_compile_commands" (builtins.readFile ./gen_compile_commands.sh)
 # writeScriptBin "gen_compile_commands" ''
 #   #!${python3}/bin/python3
 #   ${builtins.readFile ./gen_compile_commands.py}
