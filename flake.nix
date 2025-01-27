b1453c6b19e13802d1a3a63845c71cf3e07b184d
Martin-Lindbuechl@cronus <lblsolutions@outlook.com>
2025-01-27 10:43:48 +0100

    flake improvements

--- flake.nix
+++ flake.nix
@@ -3,84 +3,102 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=23.11";
+    nixpkgs-2211.url = "github:nixos/nixpkgs?ref=22.11";
   };
 
-  outputs = { self, nixpkgs, flake-utils, }@inputs:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ (import ./overlays.nix { inherit inputs; }) ];
-          };
-        in
-        {
-          packages.capstan = pkgs.capstan;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-2211,
+      flake-utils,
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import ./overlays.nix { inherit inputs; }) ];
+        };
+      in
+      #glibc-2-38 = inputs.nixpkgs-2311.legacyPackages.${pkgs.system}.glibc.all;
+      {
+        devShell = pkgs.mkShell {
 
-          devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            ack # grep tool
+            ant # java dev lib
+            autoconf
+            automake
+            bash
+            binutils
+            bisoncpp
+            gcc13
+            libgcc
+            gdb # gnu debugger
+            cmake
+            gnumake
+            gnupatch
+            gen_compile_commands
+            flamegraph # code hierarchy visualization
+            libedit
+            libgcc # Compiler
+            libtool
+            libvirt
+            lua53Packages.lua
+            ncurses
+            pax-utils # elf security library
+            p11-kit # PKCS#11 loader
+            qemu_full # hypervisor
+            readline # interactive line editing
+            unzip
+            zulu8 # Java jdk
+            clang
+            osv-ssl
+            osv-ssl-hdr
+            yaml-cpp
+            xz.out
+            krb5.out
+            libselinux
+            libz
+            boost175
+            #glibc.static
+            unixODBC
+            numactl
+            python3
+            python311Packages.requests
+            python311Packages.numpy
+            python311Packages.pandas
+            python311Packages.matplotlib
+            pkg-config
+            flex
+            bison
+          ];
 
-            # Don't add glib.static here - As there is an issue with wcslen defined twice.
-            # If you want to use statically linked executables, build them manually before calling ./scripts/build
-            nativeBuildInputs = with pkgs; [
-              ack # grep tool
-              ant # java dev lib
-              autoconf
-              automake
-              bash
-              binutils
-              bisoncpp
-              gdb # gnu debugger
-              # glibc.static
-              gnumake
-              gnupatch
-              gen_compile_commands
-              flamegraph # code hierarchy visualization
-              libedit
-              libgcc # Compiler
-              libtool
-              libvirt
-              lua53Packages.lua
-              ncurses
-              pkg-config
-              pax-utils # elf security library
-              pyright
-              python3
-              python312Packages.requests
-              python312Packages.matplotlib
-              p11-kit # PKCS#11 loader
-              qemu_full # hypervisor
-              readline # interactive line editing
-              unzip
-              zulu8 # Java jdk
-              flex
-              bison
-            ];
+          buildInputs = with pkgs; [
+            osv-boost # C++ libraries
+            readline # interactive line editing
+            libaio # I/O library
+            osv-ssl # SSL/TLS library
+            clang-tools # language server
+          ];
 
-            buildInputs = with pkgs; [
-              osv-boost # C++ libraries
-              readline # interactive line editing
-              libaio # I/O library
-              openssl # SSL/TLS library
-              clang-tools_18 # language server
-            ];
+          LD_LIBRARY_PATH = "${pkgs.readline}/lib";
+          LUA_LIB_PATH = "${pkgs.lua53Packages.lua}/lib";
+          GOMP_DIR = pkgs.libgcc.lib;
+          boost_base = "${pkgs.osv-boost}";
+          BOOST_SO_DIR = "${pkgs.boost175}/lib";
+          OPENSSL_DIR = "${pkgs.osv-ssl}";
+          OPENSSL_HDR = "${pkgs.osv-ssl-hdr}/include";
+          KRB5_DIR = "${pkgs.krb5.out}";
+          XZ_DIR = "${pkgs.xz.out}";
+          LIBZ_DIR = "${pkgs.libz}";
 
-            LD_LIBRARY_PATH = "${pkgs.readline}/lib";
-            LUA_LIB_PATH = "${pkgs.lua53Packages.lua}/lib";
-            GOMP_DIR = pkgs.libgcc.lib;
+          CAPSTAN_QEMU_PATH = "${pkgs.qemu}/bin/qemu-system-x86_64";
 
-            CAPSTAN_QEMU_PATH = "${pkgs.qemu}/bin/qemu-system-x86_64";
-
+          /*
             shellHook = ''
-              # /bin/bash --version >/dev/null 2>&1 || {
-              #   echo >&2 "Error: /bin/bash is required but was not found.  Aborting."
-              #   echo >&2 "If you're on NixOs, consider using https://github.com/Mic92/envfs."
-              #   exit 1
-              #   }
-
-              export OSV_BASE=$(git rev-parse --show-toplevel)
-              export OSV_BUILD_PATH=$OSV_BASE/build/release.x64
-
               mkdir $TMP/openssl-all
               ln -rsf ${pkgs.openssl}/* $TMP/openssl-all
               ln -rsf ${pkgs.openssl.dev}/* $TMP/openssl-all
@@ -88,12 +106,19 @@
               export OPENSSL_DIR="$TMP/openssl-all";
               export OPENSSL_LIB_PATH="$TMP/openssl-all/lib";
 
-              mkdir $TMP/libboost
-              export boost_base="$TMP/libboost"
-              ln -s ${pkgs.osv-boost}/lib/* $TMP/libboost/
-              for file in $TMP/libboost/*-x64*; do mv "$file" "''${file//-x64/}"; done
             '';
-          };
-        }
-      );
+          */
+        };
+      }
+    );
 }
+#/bin/bash --version >/dev/null 2>&1 || {
+# echo >&2 "Error: /bin/bash is required but was not found.  Aborting."
+#echo >&2 "If you're on NixOs, consider using https://github.com/Mic92/envfs."
+#exit 1
+#}
+
+#mkdir $TMP/libboost
+#ln -s ${pkgs.osv-boost}/lib/* $TMP/libboost/
+#for file in $TMP/libboost/*-x64*; do mv "$file" "''${file//-x64/}"; done
+#export boost_base="$TMP/libboost"
--- overlays.nix
+++ overlays.nix
@@ -6,5 +6,7 @@ final: _prev: {
     enableStatic = true;
     enableShared = false;
   };
+  osv-ssl = inputs.nixpkgs-2211.legacyPackages.${_prev.system}.openssl_1_1.out;
+  osv-ssl-hdr = inputs.nixpkgs-2211.legacyPackages.${_prev.system}.openssl_1_1.dev;
   gen_compile_commands = _prev.callPackage ./pkgs/gen_compile_commands.nix { };
 }
