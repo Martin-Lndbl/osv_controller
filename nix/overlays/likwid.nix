30852df1b99bac0d7fecf04d2f27d02ef401b8ef
Martin Lindbuechl <martin.lindbuechl@tum.de>
2026-05-01 15:14:43 +0000

    fix relative paths

--- nix/overlays/likwid.nix
+++ nix/overlays/likwid.nix
@@ -6,10 +6,11 @@ self: super: {
       rev = "c85d659a7a308816a3cfd987ad0b8ec63d30901f";
       hash = "sha256-AWJ8OPnhWNdYmPLxC+xJQ1VkbydujU0OKZsz93FCYRQ=";
     };
+    DEBUGFLAGS = "-fdebug-prefix-map=.=${src} -fdebug-prefix-map=/build/source=${src}";
     makeFlags = (oldAttrs.makeFlags or [ ]) ++ [
       "DEBUG=true"
-      ''CFLAGS="-fdebug-prefix-map=/build/source/=${src}/"''
-      ''CXXFLAGS="-fdebug-prefix-map=/build/source/=${src}/"''
+      "CFLAGS=$(DEBUGFLAGS)"
+      "CXXFLAGS=$(DEBUGFLAGS)"
     ];
     dontStrip = true;
   });
